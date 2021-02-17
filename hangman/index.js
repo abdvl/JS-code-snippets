794d6301097d5ad1e494024a3f8dfeacfb7fc13c
Aikepaer Abuduweili <abdvls@gmail.com>
2021-02-16 23:32:12 -0800

    update

--- hangman/index.js
+++ hangman/index.js
@@ -1,83 +1,107 @@
 const store = {
-    game : {
-        hint:"xxx",
-        guess_count:10,
-        word:"python"
+    game: {
+        hint: "xxx",
+        guess_count: 10,
+        word: "python"
     },
-    status:{
-        status:"in-progress",
-        guess:[],
-        word:"",
-        show_hint:false
+    status: {
+        status: "in-progress",
+        guess: [],
+        word: "",
+        show_hint: false
     }
 }
 
 
-
-const create_game = ()=>{
-    const {status,game} = store;
+const create_game = (store) => {
+    const { status, game } = store;
     // pick a game
     status.status = "in_progress"
     status.guess = [];
     status.word = "_".repeat(game.word.length)
     status.show_hint = false
+    return store
 }
 
-const guess_game = (character) =>{
-    const {status,game} = store;
-    if(status.guess.includes(character)) return 
+const guess_game = (store, character) => {
+    const { status, game } = store;
+    if (status.status == "failed") return store;
+    if (status.guess.includes(character)) return store;
+
     const word = game.word;
     status.guess.push(character)
-    status.word = word.split("").map(c => status.guess.includes(c)?c:"_").join("")
-    
-    if(status.guess.length == game.guess_count){
-        status.status = "failed"
-    }
+    status.word = word.split("").map(c => status.guess.includes(c) ? c : "_").join("")
 
-    if(status.word.indexOf("_") == -1){
-        status.status = "success"
-    }
-
+    if (status.guess.length == game.guess_count) status.status = "failed";
+    if (status.word.indexOf("_") == -1) status.status = "success";
+    return store
 }
 
-const show_hint = ()=> {
 
+function useGame() {
+    const reducer = (state, action) => {
+        const { type, payload } = action
+        if (type == "guess") return guess_game({ ...state }, payload)
+        if (type == "restart") return create_game({ ...state })
+        if (type == "show_hint") {
+            state.status.show_hint = true
+            return { ...state }
+        }
+    }
+    const [state, dispatch] = React.useReducer(reducer, store);
+    return [state, dispatch]
 }
 
-// function html(parts,...args){
-//     return parts.reduce((acc,part,index)=>{
-//         const arg = args[index] || "";
-//         return acc + part + (Array.isArray(arg)? arg.join(""):arg)
-//     }, "")
-// }
 
+function Buttons(props) {
+    const handleClick = (e) => {
+        const val = e.target.getAttribute("data-value")
+        props.onSelect(val)
+    }
+    const buttons = Array(26).fill(97).map((n, i) => {
+        const c = String.fromCharCode(n + i);
+        const disabled = props.status.guess.includes(c) && "disabled"
+        return <button disabled={disabled} key={c} data-value={c}>{c}</button>
 
-// function buildGameUI(store){
-//     const {status,game} = store;
-//     const buttons = Array(26).fill(97).map((n, i)=> {
-//         const c = String.fromCharCode(n+i);
-//         const disabled = status.guess.includes(c) && "disabled"
-//         return html`<button @click=${c}  ${disabled}>${c}</button>`}
-//     )
-//     const hint = status.show_hint ?"": html`
-//         <h2>${game.hint}</h2>
-//     `
-//     return html`
-//         ${hint}
-//         <div >${buttons}</div>
+    })
+    return (
+        <div onClick={handleClick}>
+            {buttons}
+        </div>
+    )
+}
 
-//     `
-// }
+function Game() {
+    const [state, dispatch] = useGame();
+    const { game, status } = state;
+    const guess = (c) => {
+        dispatch({
+            type: "guess",
+            payload: c
+        })
+    }
 
+    React.useEffect(() => {
+        dispatch({ type: "restart" })
+    }, [])
 
+    return (
+        <div>
+            <h1>Count : {game.guess_count - status.guess.length}</h1>
+            <p> {status.status}</p>
+            {status.show_hint && <p>{game.hint}</p>}
+            <p>{status.word}</p>
+            <Buttons status={status} onSelect={guess}></Buttons>
+            <button onClick={() => { dispatch({ type: "restart" }) }}>Restart</button>
+            <button onClick={() => { dispatch({ type: "show_hint" }) }}>Show hint</button>
+        </div>
+    )
+}
 
-create_game()
-guess_game("g")
-guess_game("p")
-guess_game("o")
-
-
-
-
-console.log(store, store.status.word)
-
+function App(root) {
+    ReactDOM.render(
+        <Game></Game>,
+        root
+    )
+}
+App(document.querySelector("#app"))
